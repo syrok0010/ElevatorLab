4b347a9d82566e701c12da3f3bc5665bfe87c656
syrok0010 <steamruler@mail.ru>
2024-05-18 22:05:14 +0300

    Minor refactor

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main {
     public static void main(String[] args) {
         int elevatorCount = 2;
         var building = new Building(elevatorCount);
-        var gennadiy = new RequestGenerator(building);
+        var generator = new RequestGenerator(building);
         var buildingUI = new BuildingUI(building);
         List<ElevatorListener> listeners = new ArrayList<>();
         listeners.add(buildingUI);
-        listeners.add(gennadiy);
+        listeners.add(generator);
         building.addListeners(listeners);
         for (int i = 1; i <= elevatorCount; i++) {
             buildingUI.onElevatorStatusChange(i, 0, Elevator.ElevatorState.IDLE);
         }
         buildingUI.setVisible(true);
 
-        var genThread = new Thread(gennadiy::generateRequests);
+        var genThread = new Thread(generator::generateRequests);
         genThread.start();
     }
 }
